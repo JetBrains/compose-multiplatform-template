29fb5bcb42ff3dc104df120a6315410553070fe0
dima.avdeev <dima.avdeev@jetbrains.com>
2023-08-20 16:39:55 +0400

    Preview works on Android

--- androidApp/src/main/kotlin/com/myapplication/MainActivity.kt
+++ androidApp/src/main/kotlin/com/myapplication/MainActivity.kt
@@ -4,6 +4,8 @@ import MainView
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,4 +15,10 @@ class MainActivity : AppCompatActivity() {
             MainView()
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MainView()
 }
