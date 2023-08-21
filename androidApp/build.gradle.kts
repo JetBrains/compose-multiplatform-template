46f03c632b3a588315df519c9d92fbabcbb9a4c5
dima.avdeev <dima.avdeev@jetbrains.com>
2023-08-21 08:57:42 +0400

    Revert "Preview works on Android"
    This reverts commit 29fb5bcb42ff3dc104df120a6315410553070fe0.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,32 +1,25 @@
 plugins {
-//    kotlin("multiplatform")
+    kotlin("multiplatform")
     id("com.android.application")
-//    id("org.jetbrains.compose")
-    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.compose")
 }
 
-//kotlin {
-//    androidTarget()
-//    sourceSets {
-//        val androidMain by getting {
-//            dependencies {
-//                implementation(project(":shared"))
-//            }
-//        }
-//    }
-//}
+kotlin {
+    androidTarget()
+    sourceSets {
+        val androidMain by getting {
+            dependencies {
+                implementation(project(":shared"))
+            }
+        }
+    }
+}
 
 android {
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
     compileSdk = (findProperty("android.compileSdk") as String).toInt()
     namespace = "com.myapplication"
 
-    sourceSets["main"].manifest.srcFile("src/main/AndroidManifest.xml")
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
 
     defaultConfig {
         applicationId = "com.myapplication.MyApplication"
@@ -42,18 +35,4 @@ android {
     kotlin {
         jvmToolchain(11)
     }
-}
-
-dependencies {
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-
-    implementation(project(":shared"))
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- androidApp/src/androidMain/kotlin/com/myapplication/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/myapplication/MainActivity.kt
@@ -4,8 +4,6 @@ import MainView
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +13,4 @@ class MainActivity : AppCompatActivity() {
             MainView()
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MainView()
 }
