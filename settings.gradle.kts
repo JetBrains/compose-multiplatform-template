74108b29bdf2dfe32e5702bd6fa5e6da009968f1
avan1235 <macieekprocyk@gmail.com>
2023-09-16 14:42:04 +0200

    Migrate build to version catalogs with libs.versions.toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,19 +11,6 @@ pluginManagement {
         google()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-
-    plugins {
-        val kotlinVersion = extra["kotlin.version"] as String
-        val agpVersion = extra["agp.version"] as String
-        val composeVersion = extra["compose.version"] as String
-
-        kotlin("multiplatform").version(kotlinVersion)
-
-        id("com.android.application").version(agpVersion)
-        id("com.android.library").version(agpVersion)
-
-        id("org.jetbrains.compose").version(composeVersion)
-    }
 }
 
 plugins {
