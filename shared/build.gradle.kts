7db5e5073585663b898ddc68925a83457327aa79
avan1235 <macieekprocyk@gmail.com>
2023-09-16 14:03:42 +0200

    Update AndroidX Core Ktx version to 1.12.0

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -34,7 +34,7 @@ kotlin {
             dependencies {
                 api("androidx.activity:activity-compose:1.7.2")
                 api("androidx.appcompat:appcompat:1.6.1")
-                api("androidx.core:core-ktx:1.10.1")
+                api("androidx.core:core-ktx:1.12.0")
             }
         }
         val iosX64Main by getting
