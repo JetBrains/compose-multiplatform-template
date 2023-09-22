0154f45a56c13bf2747e703f094c87510f063b26
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-09-22 12:14:04 +0200

    Try K2 with kotlin 1.9.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,25 @@ plugins {
     id("com.android.library").apply(false)
     id("org.jetbrains.compose").apply(false)
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        mavenLocal()
+    }
+
+    afterEvaluate {
+        val project = this
+        val compilerPluginVersion = project.properties["compose.kotlinCompilerPluginVersion"] as? String
+        val kotlinVersion = project.properties["kotlin.version"] as? String
+        project.extensions.findByType<org.jetbrains.compose.ComposeExtension>()?.also {
+            if (!compilerPluginVersion.isNullOrEmpty()) {
+                println("${project.name} is using compilerPluginVersion = $compilerPluginVersion")
+                it.kotlinCompilerPlugin.set(compilerPluginVersion)
+                it.kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=$kotlinVersion")
+            }
+        }
+    }
+}
