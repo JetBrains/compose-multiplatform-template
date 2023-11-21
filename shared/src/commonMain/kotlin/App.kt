c788b21d4230cd5349b690b6ef5a0ce1ec32de18
mlenoir4 <mlenoir4@myges.fr>
2023-11-21 07:59:40 +0100

    Frist push

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,41 +1,36 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
+    var questionsProgress by remember { mutableStateOf(0) }
+
     MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello, World!") }
-        var showImage by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                greetingText = "Hello, ${getPlatformName()}"
-                showImage = !showImage
-            }) {
-                Text(greetingText)
-            }
-            AnimatedVisibility(showImage) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    contentDescription = "Compose Multiplatform icon"
-                )
-            }
-        }
+        val quiz =  Quiz(listOf(
+            Question(
+                1,
+                "Android is a great platform ?",
+                1,
+                listOf(Answer(1, "YES"), Answer(2, "NO"), Answer(3, "HIHIHI"))
+            ),
+            Question(
+                1,
+                "Android is a bad platform ?",
+                2,
+                listOf(Answer(1, "YES"), Answer(2, "NO"))
+            )
+        ))
+        //WelcomeScreen()
+        //ScoreScreen("10/20")
+        questionScreen(quiz)
     }
 }
+
 
 expect fun getPlatformName(): String
---
+++ shared/src/commonMain/kotlin/WelcomeScreen.kt
@@ -0,0 +1,50 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+@Composable()
+internal fun welcomeScreen(){
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxWidth().fillMaxHeight()
+    ) {
+        Card(
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.padding(10.dp),
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = "Quiz",
+                        fontSize = 30.sp,
+                        modifier = Modifier.padding(all = 10.dp)
+                    )
+                    Text(
+                        modifier = Modifier.padding(all = 10.dp),
+                        text = "A simple Quiz to discovers KMP and compose.",
+                    )
+                    Button(
+                        modifier = Modifier.padding(all = 10.dp),
+                        onClick = {  }
+
+                    ) {
+                        Text("Start the Quiz")
+                    }
+                }
+            }
+        }
+    }
+}
